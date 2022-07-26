abea78fb921d5f60e4ba593974c41ffd22b4d25f
gyj <yujie.guan@wekeystudio.com>
2022-07-26 16:30:33 +0800

    update:V0.7 读写分离模型实现

--- znet/connection.go
+++ znet/connection.go
@@ -13,7 +13,8 @@ type Connection struct {
 	ConnID     uint32
 	isClosed   bool
 	ExitChan   chan bool
-	MsgHandler ziface.IMsgHandler
+	msgChan    chan []byte        //无缓冲通道,用于读写goroutine之间的消息通信
+	MsgHandler ziface.IMsgHandler //消息的管理msgId和对应处理业务API的关系
 }
 
 func NewConnection(conn *net.TCPConn, connID uint32, handler ziface.IMsgHandler) *Connection {
@@ -22,13 +23,14 @@ func NewConnection(conn *net.TCPConn, connID uint32, handler ziface.IMsgHandler)
 		ConnID:     connID,
 		isClosed:   false,
 		MsgHandler: handler,
+		msgChan:    make(chan []byte),
 		ExitChan:   make(chan bool, 1),
 	}
 }
 
 func (c *Connection) StartReader() {
-	fmt.Println("Reader Goroutine is running...")
-	defer fmt.Println("connId = ", c.ConnID, " Reader id exit,remote addr is ", c.RemoteAddr().String())
+	fmt.Println("[Reader Goroutine is running...]")
+	defer fmt.Println("[connId = ", c.ConnID, " ,Reader exit,remote addr is ", c.RemoteAddr().String(), "]")
 	defer c.Stop()
 
 	for {
@@ -67,9 +69,33 @@ func (c *Connection) StartReader() {
 	}
 }
 
+//StartWriter 写消息的goroutine,专门发送给客户端消息的模块
+func (c *Connection) StartWriter() {
+	fmt.Println("[Writer Goroutine is running...]")
+	defer fmt.Println("[connId = ", c.ConnID, " ,Writer exit,remote addr is ", c.RemoteAddr().String(), "]")
+	defer close(c.msgChan)
+
+	//不断阻塞等待channel消息
+	for {
+		select {
+		case data := <-c.msgChan:
+			if _, err := c.Conn.Write(data); err != nil {
+				fmt.Println("Send data error,", err)
+				return
+			}
+		case <-c.ExitChan:
+			//代表Reader已经退出,Writer也退出
+			return
+		}
+	}
+}
+
 func (c *Connection) Start() {
 	fmt.Println("Conn Start().. ConnID = ", c.ConnID)
+	//启动从当前连接的读数据业务
 	go c.StartReader()
+	//启动从当前连接的写数据业务
+	go c.StartWriter()
 }
 
 func (c *Connection) Stop() {
@@ -78,8 +104,11 @@ func (c *Connection) Stop() {
 	if c.isClosed {
 		return
 	}
+
 	c.isClosed = true
+	//关闭socket连接
 	c.Conn.Close()
+
 	close(c.ExitChan)
 }
 
@@ -107,10 +136,6 @@ func (c *Connection) SendMsg(msgId uint32, data []byte) error {
 		return err
 	}
 
-	_, err = c.Conn.Write(binary)
-	if err != nil {
-		fmt.Println("write msg error", err)
-		return err
-	}
+	c.msgChan <- binary
 	return nil
 }
